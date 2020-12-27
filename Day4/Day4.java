aad08bd8b4aed667230a1389d6a10f9216d3026d
applejuice0504 <zwickler.michael@googlemail.com>
2020-12-27 13:06:31 +0100

    Day 4 part2 added

--- Day4/Day4.java
+++ Day4/Day4.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File; 
 import java.io.FileNotFoundException;
+import java.util.regex.Pattern;
+
 
 //https://adventofcode.com/2020/day/4
 public class Day4 {
@@ -10,7 +12,6 @@ public class Day4 {
 		ArrayList<String> input = readInput("input.txt");
 		
 		int validCount = 0;
-		
 		//Check passport validity for Part 1 - 7 Fields existing
 		for (String str : input) {
 			int fieldCount = 0;
@@ -26,11 +27,56 @@ public class Day4 {
 		} 
 		System.out.println("Number of Valid passports [Part 1]: " + validCount + " of " + input.size() + " passports are ok!");
 		
-		
+				
+		int validCount2 = 0;
 		//Check passport validity for Part 2 - if the 7 fields have correct entries
 		for (String str : input) {
+			int fieldCount = 0;			
+						
+			//Check if byr is between 1920 and 2002
+			if ( str.matches(".*byr:19[2-9][0-9]\\W.*") || str.matches(".*byr:200[0-2]\\W.*"))
+				fieldCount += 1; 
 			
+			//Check if iyr is between 2010 and 2020
+			if ( str.matches(".*iyr:201[0-9]\\W.*") || str.matches(".*iyr:2020\\W.*"))
+				fieldCount += 1; 
+			
+			//Check if eyr is between 2020 and 2030
+			if ( str.matches(".*eyr:202[0-9]\\W.*") || str.matches(".*eyr:2030\\W.*"))
+				fieldCount += 1;
+			
+			//Check if hcl is # + six character 0-9 or a-f
+			if ( str.matches(".*hcl:#[0-9a-f]{6}\\W.*") )
+				fieldCount += 1; 		
+					
+			//Check if pid is a nine digit number
+			if ( str.matches(".*pid:[0-9]{9}\\W.*") )
+				fieldCount += 1; 
+			
+			//Check ecl is one of amb blu brn gry grn hzl oth
+			if ( str.matches(".*ecl:amb\\W.*") || 
+				 str.matches(".*ecl:blu\\W.*") ||
+				 str.matches(".*ecl:brn\\W.*") ||
+			     str.matches(".*ecl:gry\\W.*") ||
+			     str.matches(".*ecl:grn\\W.*") ||
+				 str.matches(".*ecl:hzl\\W.*") ||
+				 str.matches(".*ecl:oth\\W.*") )
+					 fieldCount += 1;
+			
+			//Check if hgt a number followed by cm or in
+	        	//If cm, the number must be at least 150 and at most 193.
+	        	//If in, the number must be at least 59 and at most 76.
+			if ( str.matches(".*hgt:1[5-8][0-9]cm\\W.*") || str.matches(".*hgt:1[9][0-3]cm\\W.*"))
+				fieldCount +=1;
+			else if ( str.matches(".*hgt:59in\\W.*") || 
+					  str.matches(".*hgt:6[0-9]in\\W.*") || 
+					  str.matches(".*hgt:7[0-6]in\\W.*") )
+						  fieldCount += 1;
+			
+			if (fieldCount == 7) 
+				validCount2 += 1;			
 		}
+		System.out.println("Number of Valid passports [Part 2]: " + validCount2 + " of " + input.size() + " passports are ok!");
 		
 		
 	}
@@ -57,7 +103,7 @@ public class Day4 {
 		String s = "";
 		for (String str : temp) {
 			if ( ! str.isEmpty() ) 
-				s = s + " " + str;
+				s = s + " " + str + " ";
 			else {
 				list.add(s);
 				s = "";
